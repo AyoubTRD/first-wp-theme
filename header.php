210fb7481363419bfa74c7c0375178cceb272116
Ayoub Taouarda <ayoubtrdone@gmail.com>
2020-06-16 21:05:58 +0100

    first-wp-theme

---
+++ header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html <?php language_attributes() ?>>
+<head>
+  <meta charset="<?php bloginfo("charset") ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>First theme</title>
+  <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+
+</head>
+<body>
